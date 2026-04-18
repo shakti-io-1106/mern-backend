781f1ff628826e29c153a643cb59eba5184e3dd3
shakti-io-1106 <shaktipada.sps@gmail.com>
2026-04-18 19:44:11 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors'
+const app = express();
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+import todoRoutes from './routes/todo.route.js'
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+app.use(express.json());
+//app.use(cors());
+app.use('/api/todos',todoRoutes);
+
+/*const __dirname = path.resolve();
+if(process.env.NODE.ENV === "productions"){
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    });
+}*/
+
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log("Server Started at http://localhost:5000");
+})
