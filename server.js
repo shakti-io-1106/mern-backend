95fbed029f30908e08538e284bf24c70be484d96
shakti-io-1106 <shaktipada.sps@gmail.com>
2026-04-18 21:23:45 +0530

    cors fix

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json());
-//app.use(cors());
+app.use(
+  cors({
+    origin: "https://mern-frontend-shakti7.vercel.app",
+  }),
+);
 app.use("/api/todos", todoRoutes);
 
 /*const __dirname = path.resolve();
